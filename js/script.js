05661660f8b80c0378b04dd6b7d3387d9cfb85eb
unknown <egbert.fewi@gmail.com>
2023-08-15 21:30:58 +0700

    Menambahkan css dan js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const luas = document.queryselector(".luas")
+const keliling = document.queryselector(".keliling")
+const form = document.queryselector("#form")
+const switchs = document.queryselector(".switch")
+
+function tab2() {
+    form.style.marginleft = "-100%";
+}
